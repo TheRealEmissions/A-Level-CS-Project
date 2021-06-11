ad5d339f407eeec323642255459a10ff8341361b
TheRealEmissions <me@liammartin.site>
2021-06-11 10:15:24 +0100

    Added AccountHandler class

---
+++ The Project/Accounts/AccountHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace The_Project.Accounts
+{
+    public class AccountHandler
+    {
+        private SqlConnection? SqlConnection = null;
+        public AccountHandler(string Username, string Password)
+        {
+            SqlConnection = StartSQLConnection();
+        }
+
+        public AccountHandler(string Username, string Password, string ConfPassword)
+        {
+
+        }
+
+        public SqlConnection StartSQLConnection()
+        {
+            string ConnectionString = "";
+            SqlConnection connection = new(ConnectionString);
+            connection.Open();
+        }
+    }
+}
