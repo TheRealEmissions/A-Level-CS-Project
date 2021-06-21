00c78afcb852932a0f8d4209b6e12e1509779e1b
TheRealEmissions <me@liammartin.site>
2021-06-21 10:53:22 +0100

    Added Account class

---
+++ The Project/Accounts/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Accounts
+{
+    class Account : AccountHandler
+    {
+        public Account(string Username, string Password) : base(Username, Password)
+        {
+
+        }
+
+        public Account(string Username, string Password, string ConfPassword) : base(Username, Password, ConfPassword)
+        {
+
+        }
+    }
+}
