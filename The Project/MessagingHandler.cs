176bc327dea019850016d3aed88141947250a97b
TheRealEmissions <me@liammartin.site>
2021-06-21 11:18:45 +0100

    Added MessagingHandler class

---
+++ The Project/MessagingHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Accounts;
+using The_Project.Database;
+
+#nullable enable
+namespace The_Project
+{
+    public class MessagingHandler : SQL
+    {
+        public Account? UserAccount { get; set; } = null;
+        public Recipient? Recipient { get; set; } = null;
+
+        public MessagingHandler() : base()
+        {
+
+        }
+    }
+}
