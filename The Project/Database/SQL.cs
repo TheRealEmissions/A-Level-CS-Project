235d8845f4199f247a6a5d1e41ae164dc7dece47
TheRealEmissions <me@liammartin.site>
2021-06-21 11:18:22 +0100

    Added SQL class

---
+++ The Project/Database/SQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace The_Project.Database
+{
+    public class SQL
+    {
+        public SqliteConnection? Connection = null;
+        public SQL()
+        {
+            Connection = Start();
+        }
+
+        private SqliteConnection Start()
+        {
+            SqliteConnection Connection = new("Data Source=account.db;Mode=ReadWriteCreate");
+            Connection.Open();
+            return Connection;
+        }
+    }
+}
