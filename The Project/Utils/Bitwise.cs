3985ae9434a75535e7f7d0490c800b1dcfd2c95a
Emissions <unitedrealm@hotmail.com>
2021-07-22 21:08:28 +0100

    Added Bitwise

---
+++ The Project/Utils/Bitwise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Utils
+{
+    public class Bitwise
+    {
+        public static BitArray RightRotate(uint bits)
+        {
+            // todo
+        }
+
+        public static BitArray LeftRotate(uint bits)
+        {
+            // todo
+        }
+    }
+}
