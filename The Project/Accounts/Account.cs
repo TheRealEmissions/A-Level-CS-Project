493f60f20ca7c376339793c3ab21aea413a56178
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-02 02:28:39 +0100

    Formatted Account.cs

--- The Project/Accounts/Account.cs
+++ The Project/Accounts/Account.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace The_Project.Accounts
 {
     public class Account : AccountHandler
     {
         public string Username = "";
+
         public Account(string Username, string Password, SqliteConnection Connection) : base(Username, Password, Connection)
         {
             this.Username = Username;
