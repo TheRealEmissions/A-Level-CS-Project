90f76ab096ca134bed4f8c610e4e0e48ff0a88bc
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-07 15:47:20 +0100

    Added Encryption

---
+++ The Project/Cryptography/Encryption.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Extensions;
+
+namespace The_Project.Cryptography
+{
+    public struct PublicKey
+    {
+        public BigInteger n;
+        public BigInteger e;
+
+        public PublicKey(BigInteger n, BigInteger e)
+        {
+            this.n = n;
+            this.e = e;
+        }
+    }
+
+    internal struct PrivateKey
+    {
+        public BigInteger n;
+        public BigInteger d;
+
+        public PrivateKey(BigInteger n, BigInteger d)
+        {
+            this.n = n;
+            this.d = d;
+        }
+    }
+
+    public static class Encryption
+    {
+        private static readonly BigInteger x = GeneratePrime();
+        private static readonly BigInteger y = GeneratePrime();
+        private static readonly BigInteger n = x * y;
+
+        private static readonly BigInteger phi = (x - 1) * (y - 1);
+
+        private static readonly BigInteger e = GetCoprimeToPhi();
+        private static readonly BigInteger d = GetD();
+
+        public static readonly PublicKey Public = new(n, e);
+        private static readonly PrivateKey Private = new(n, d);
+
+        private static BigInteger GetD()
+        {
+            BigInteger d = (1 + phi) / e;
+            int i = 1;
+            while (e*d%phi != (1%phi))
+            {
+                d = (1 + ((i + 1) * phi)) / e;
+                i++;
+            }
+            return d;
+        }
+
+        // find e such that e > 1, e < phi
+        // e is co-prime to phi
+        private static BigInteger GetCoprimeToPhi()
+        {
+            BigInteger e = 2;
+            while (e < phi)
+            {
+                if (GreatestCommonDivider(e, phi) == 1) break;
+                e++;
+            }
+            return e;
+        }
+
+        private static BigInteger GreatestCommonDivider(BigInteger a, BigInteger b)
+        {
+            BigInteger temp = a % b;
+            if (temp == 0) return b;
+            return GreatestCommonDivider(b, temp);
+        }
+
+        private static BigInteger GeneratePrime(int bits = 8)
+        {
+            byte[] Byte = new byte[bits / 8];
+            new Random().NextBytes(Byte);
+            BitArray bitArray = new(Byte);
+            bitArray.Set(0, true);
+            bitArray.Set(bits - 1, true);
+            bitArray.CopyTo(Byte, 0);
+            BigInteger Prime = new(Byte, true);
+            return Prime.IsPrime() ? Prime : GeneratePrime(bits);
+        }
+
+        private static bool IsPrime(this BigInteger n, int k=128)
+        {
+            if (n <= 1 || n % 2 == 0) return false;
+            if (n == 3) return true;
+
+            // O(n^2) - improve
+            for (uint i = 3; i < Math.Sqrt((double)n); i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static string Encrypt(this string s, PublicKey Key)
+        {
+            byte[] StringBytes = Encoding.UTF8.GetBytes(s.ToCharArray()).ToArray();
+            uint[] Numbers = StringBytes.Select(x => (uint)x).ToArray();
+            uint[] CipheredNumbers = Numbers.Select(x => (uint)BigInteger.ModPow(x, Key.e, Key.n)).ToArray();
+            string[] Ciphered = CipheredNumbers.Select(x => x.ToString("X")).ToArray();
+            string Cipher = string.Join('-', Ciphered);
+            return Cipher;
+        }
+
+        public static string Decrypt(this string s)
+        {
+            string[] Ciphered = s.Split('-');
+            uint[] CipheredNumbers = Ciphered.Select(x => Convert.ToUInt32(x, 16)).ToArray();
+            uint[] Numbers = CipheredNumbers.Select(x => (uint)BigInteger.ModPow(x, Private.d, Private.n)).ToArray();
+            byte[] CharBytes = Numbers.Select(x => (byte)x).ToArray();
+            char[] Characters = Encoding.UTF8.GetChars(CharBytes);
+            return new string(Characters);
+        }
+    }
+}
