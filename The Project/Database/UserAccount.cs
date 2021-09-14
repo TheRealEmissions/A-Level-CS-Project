23fd50e8dcf6ee26d446cd9218bed32ec18d75c5
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-14 10:09:52 +0100

    Added UserAccount (db)

---
+++ The Project/Database/UserAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Database
+{
+    class UserAccount : IDatabaseAccount
+    {
+
+        public void CreateEntry()
+        {
+
+        }
+    }
+}
