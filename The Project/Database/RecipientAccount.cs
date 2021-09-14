0c8f81c1dad8af044df9c18e773bd728e5b83f51
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-14 10:09:40 +0100

    Added RecipientAccount (db)

---
+++ The Project/Database/RecipientAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Database
+{
+    public class RecipientAccount : IDatabaseAccount
+    {
+        SqliteConnection Connection;
+
+        public RecipientAccount(SqliteConnection Connection)
+        {
+            this.Connection = Connection;
+        }
+
+        public RecipientAccount()
+        {
+            Connection = new SQL().Connection;
+        }
+
+        public void CreateEntry()
+        {
+
+        }
+    }
+}
