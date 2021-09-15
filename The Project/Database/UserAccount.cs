07d349689faa017b3eb03a1cfe0a2363c50bd0de
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-15 10:02:31 +0100

    Updated UserAccount

--- The Project/Database/UserAccount.cs
+++ The Project/Database/UserAccount.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using The_Project.Database.Interfaces;
 
 namespace The_Project.Database
 {
-    class UserAccount : IDatabaseAccount
+    class UserAccount : IDatabaseUserAccount
     {
+        SqliteConnection Connection;
+
+        public UserAccount(SqliteConnection Connection)
+        {
+            this.Connection = Connection;
+        }
+
+        public UserAccount()
+        {
+            Connection = new SQL().Connection;
+        }
+
+        public bool ComparePassword(string Password)
+        {
+            string DbPassword = GetPassword();
+            return DbPassword == Password;
+        }
 
         public void CreateEntry()
         {
 
+        }
+
+        public string GetPassword()
+        {
+            throw new NotImplementedException();
         }
     }
 }
