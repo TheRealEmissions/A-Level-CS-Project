ee5fb3a65b76aefa3ea3daa533f6722ef7ed9b08
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:16:12 +0100

    Added Tables

---
+++ The Project/Database/Tables/Tables.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Database.Tables.Interfaces;
+
+namespace The_Project.Database.Tables
+{
+    public class Tables
+    {
+        private List<ISQLTable> AllTables = new();
+
+        public Tables(SqliteConnection Connection)
+        {
+            AllTables.Add(new Messages(Connection));
+            AllTables.Add(new RecipientAccount(Connection));
+            AllTables.Add(new RecipientAddresses(Connection));
+            AllTables.Add(new UserAccount(Connection));
+        }
+
+        public ISQLTable[] GetAndCreateAllTables() => AllTables.ToArray();
+    }
+}
