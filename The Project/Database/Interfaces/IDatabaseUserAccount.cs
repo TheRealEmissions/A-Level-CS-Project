6d92e917e345b69ea6d8be32bf25ae5ce8b9fcec
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:35 +0100

    Added IDatabaseUserAccount

---
+++ The Project/Database/Interfaces/IDatabaseUserAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Database.Interfaces
+{
+    /**
+     * REQUIRED:
+     * check password (based on hashes)
+     */
+    interface IDatabaseUserAccount : IDatabaseAccount
+    {
+        public bool ComparePassword(string Password);
+        public string GetPassword();
+        public void SetPassword();
+    }
+}
