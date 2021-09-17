01ffc8143879c493918ad63f62705cc40baef129
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:16:18 +0100

    Added UserAccount

---
+++ The Project/Database/Tables/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Database.Tables.Interfaces;
+using The_Project.Extensions;
+
+namespace The_Project.Database.Tables
+{
+    public class UserAccount : MustConstructWith<SqliteConnection>, ISQLTable
+    {
+        protected SqliteConnection Connection;
+
+        public UserAccount(SqliteConnection connection) : base(connection)
+        {
+            Connection = connection;
+            CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqliteCommand Command = Connection.CreateCommand();
+            Command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS db.useraccounts (
+                    username TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL,
+                    account_id TEXT PRIMARY KEY
+                )
+            ";
+            Command.ExecuteReader();
+        }
+    }
+}
