3210d5bcce2d90491d2986ddb4a94f99a180bcc5
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:16:42 +0100

    Added Constructor extension

---
+++ The Project/Extensions/Constructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Extensions
+{
+    public abstract class MustConstructWith<T>
+    {
+        public MustConstructWith(T parameters) { }
+    }
+}
