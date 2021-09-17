41777cb39f0f89e82b345fc9b3629f8d8c1ab4d3
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:16:00 +0100

    Added RecipientAccount

---
+++ The Project/Database/Tables/RecipientAccount.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Database.Tables.Interfaces;
+using The_Project.Extensions;
+
+namespace The_Project.Database.Tables
+{
+    public class RecipientAccount : MustConstructWith<SqliteConnection>, ISQLTable
+    {
+        protected SqliteConnection Connection;
+        public RecipientAccount(SqliteConnection connection) : base(connection)
+        {
+            Connection = connection;
+            CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqliteCommand Command = Connection.CreateCommand();
+            Command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS db.recipientaccounts (
+                    nickname TEXT NOT NULL,
+                    account_id TEXT NOT NULL,
+                    ref_account_id TEXT NOT NULL UNIQUE,
+                    FOREIGN KEY (ref_account_id)
+                        REFERENCES useraccounts (account_id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE
+                )
+            ";
+            Command.ExecuteReader();
+        }
+    }
+}
