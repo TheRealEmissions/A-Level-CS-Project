57415165964f4c2d6023bdf03467ab920e044ce8
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:07 +0100

    Updated Recipient

--- The Project/Accounts/Recipient.cs
+++ The Project/Accounts/Recipient.cs
@@ -1,6 +1,6 @@
 ﻿namespace The_Project.Accounts
 {
-    public class Recipient : Account, IAccount
+    public class Recipient : Account
     {
         public Recipient(string Username) : base(Username)
         {
