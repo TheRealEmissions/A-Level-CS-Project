d0d2d239b152be65ce5d6dff7bff305e2db18676
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:14 +0100

    Added UserId struct

---
+++ The Project/Accounts/UserId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Accounts
+{
+    public struct UserId
+    {
+        public IPAddress IP;
+        public int MinPort;
+        public int MaxPort;
+        public string AccountId;
+
+        public UserId(IPAddress IP, int MinPort, int MaxPort, string AccountId)
+        {
+            this.IP = IP;
+            this.MinPort = MinPort;
+            this.MaxPort = MaxPort;
+            this.AccountId = AccountId;
+        }
+    }
+}
