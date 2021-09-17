68feeb279053435dcf4cb5e6547478a9268b99bb
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:28 +0100

    Added IDatabaseAccount

---
+++ The Project/Database/Interfaces/IDatabaseAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Accounts;
+
+namespace The_Project.Database
+{
+    interface IDatabaseAccount
+    {
+        public void CreateEntry(string Username, UserId UserId);
+    }
+}
