64e452804fcb37f8762820383a35b7991207414b
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:43 +0100

    Added ISQLTable

---
+++ The Project/Database/Tables/Interfaces/ISQLTable.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Database.Tables.Interfaces
+{
+    public interface ISQLTable
+    {
+        public void CreateTable();
+    }
+}
