9ee8f1df9c2d281318c431dc99ce7dbfd59420cb
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-17 14:15:50 +0100

    Added Messages table

---
+++ The Project/Database/Tables/Messages.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Database.Tables.Interfaces;
+using The_Project.Extensions;
+
+namespace The_Project.Database.Tables
+{
+    public class Messages : MustConstructWith<SqliteConnection>, ISQLTable
+    {
+        protected SqliteConnection Connection;
+        public Messages(SqliteConnection Connection) : base(Connection)
+        {
+            this.Connection = Connection;
+            CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqliteCommand Command = Connection.CreateCommand();
+            Command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS db.messages (
+                    user_account_id TEXT NOT NULL
+                )
+            ";
+        }
+    }
+}
