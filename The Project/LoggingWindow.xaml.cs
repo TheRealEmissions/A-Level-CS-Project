766ed6f8650d26ddd879060ed64105261f79a7fb
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-23 12:28:57 +0100

    Added/updated windows

---
+++ The Project/LoggingWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_Project
+{
+    /// <summary>
+    /// Interaction logic for LoggingWindow.xaml
+    /// </summary>
+    public partial class LoggingWindow : Window
+    {
+        public List<string> OutputLog = new();
+
+        public LoggingWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Debug(string text)
+        {
+            OutputLog.Add(text);
+            if (OutputLog.Count > 100) OutputLog.RemoveAt(0);
+            txt_log.Text = string.Join("\n", OutputLog);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
--- The Project/LoginWindow.xaml.cs
+++ The Project/LoginWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace The_Project
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<string> OutputLog = new();
+        LoggingWindow DebugWindow = new();
 
         private MessagingHandler Handler = new();
         public SqliteConnection? SQLConnection = null;
@@ -38,8 +38,8 @@ namespace The_Project
             btn_login.Click += Btn_login_Click;
             btn_register.Click += Btn_register_Click;
             txtinput_username.TextChanged += Txtinput_username_TextChanged;
-            txtinput_pswd.TextChanged += Txtinput_pswd_TextChanged;
-            txtinput_confpswd.TextChanged += Txtinput_confpswd_TextChanged;
+            txtinput_pswd.PasswordChanged += Txtinput_pswd_TextChanged;
+            txtinput_confpswd.PasswordChanged += Txtinput_confpswd_TextChanged;
             txtinput_username.LostFocus += Txtinput_username_LostFocus;
             txtinput_pswd.LostFocus += Txtinput_pswd_LostFocus;
             txtinput_confpswd.LostFocus += Txtinput_confpswd_LostFocus;
@@ -48,29 +48,26 @@ namespace The_Project
             txtinput_username.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(Txtinput_username_MouseLeftButtonDown), true);
             txtinput_pswd.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(Txtinput_pswd_MouseLeftButtonDown), true);
             txtinput_confpswd.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(Txtinput_confpswd_MouseLeftButtonDown), true);
-            txtinput_testEncryption.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(txtinput_testEncryption_MouseLeftButtonDown), true);
         }
 
-        public void Output(string text)
+        public void Debug(string text)
         {
-            OutputLog.Add(text);
-            if (OutputLog.Count > 100) OutputLog.RemoveAt(0);
-            txt_log.Text = string.Join("\n", OutputLog);
+            DebugWindow.Debug(text);
         }
 
         private void Txtinput_confpswd_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (txtinput_confpswd.Text.Length < 1)
+            if (txtinput_confpswd.Password.Length < 1)
             {
-                txtinput_confpswd.Text = "Confirm Password";
+                txtinput_confpswd.Password = "Confirm Password";
             }
         }
 
         private void Txtinput_pswd_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (txtinput_pswd.Text.Length < 1)
+            if (txtinput_pswd.Password.Length < 1)
             {
-                txtinput_pswd.Text = "Password";
+                txtinput_pswd.Password = "Password";
             }
         }
 
@@ -82,17 +79,17 @@ namespace The_Project
             }
         }
 
-        private void Txtinput_pswd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Txtinput_pswd_TextChanged(object sender, RoutedEventArgs e)
         {
             if (txtinput_username.Text.Length < 1)
             {
                 SetAllButtonsToDisabled();
                 return;
             }
-            if (txtinput_pswd.Text.Length > 0 && txtinput_pswd.Text != "Password")
+            if (txtinput_pswd.Password.Length > 0 && txtinput_pswd.Password != "Password")
             {
                 btn_login.IsEnabled = true;
-                if (txtinput_confpswd.Text.Length > 0 && txtinput_confpswd.Text == txtinput_pswd.Text) btn_register.IsEnabled = true;
+                if (txtinput_confpswd.Password.Length > 0 && txtinput_confpswd.Password == txtinput_pswd.Password) btn_register.IsEnabled = true;
             }
             else
             {
@@ -100,14 +97,14 @@ namespace The_Project
             }
         }
 
-        private void Txtinput_confpswd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Txtinput_confpswd_TextChanged(object sender, RoutedEventArgs e)
         {
-            if (txtinput_pswd.Text.Length < 1)
+            if (txtinput_pswd.Password.Length < 1)
             {
                 SetAllButtonsToDisabled();
                 return;
             }
-            if (txtinput_pswd.Text == txtinput_confpswd.Text && (txtinput_confpswd.Text != "Confirm Password" || txtinput_confpswd.Text != "Password"))
+            if (txtinput_pswd.Password == txtinput_confpswd.Password && (txtinput_confpswd.Password != "Confirm Password" || txtinput_confpswd.Password != "Password"))
             {
                 btn_register.IsEnabled = true;
             }
@@ -123,20 +120,33 @@ namespace The_Project
 
         private void Btn_register_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+
+            Hashing Hash = new(this);
+
+            // generate password hash
+            string PasswordHash = Hash.Hash(txtinput_pswd.Password);
+
+            // create an account
+            Account Account = new(txtinput_username.Text, txtinput_pswd.Password, txtinput_confpswd.Password, SQLConnection);
+
+            // register account to handler
+            Handler.UserAccount = Account;
         }
 
         private void Btn_login_Click(object sender, RoutedEventArgs e)
         {
             Hashing Hash = new(this);
 
-            Output("Registered LOGIN CLICK - Finding account");
-            string PasswordHash = Hash.Hash(txtinput_pswd.Text);
-            Output($"Password hash: {PasswordHash}");
-            Output($"Hash length: {PasswordHash.Length}");
-            Account account = new(txtinput_username.Text, txtinput_pswd.Text, SQLConnection);
+            Debug("Registered LOGIN CLICK - Finding account");
+            string PasswordHash = Hash.Hash(txtinput_pswd.Password);
+            Debug($"Password hash: {PasswordHash}");
+            Debug($"Hash length: {PasswordHash.Length}");
+            Account account = new(txtinput_username.Text, txtinput_pswd.Password, SQLConnection);
             Handler.UserAccount = account;
-            Output("FOUND ACCOUNT!");
+            Debug("FOUND ACCOUNT!");
+
+            UserConnectionPage UserConnectionPage = new();
+            this.Content = UserConnectionPage;
         }
 
         public void SetAllButtonsToDisabled()
@@ -152,43 +162,18 @@ namespace The_Project
 
         private void Txtinput_pswd_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            txtinput_pswd.Text = string.Empty;
+            txtinput_pswd.Password = string.Empty;
         }
 
         private void Txtinput_confpswd_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            txtinput_confpswd.Text = string.Empty;
+            txtinput_confpswd.Password = string.Empty;
         }
 
-        private void txtinput_testEncryption_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Btn_DebugWindow_Click(object sender, RoutedEventArgs e)
         {
-            txtinput_testEncryption.Text = string.Empty;
-        }
-
-        private void txtinput_testEncryption_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-
-                Output($"Text to encrypt -> {txtinput_testEncryption.Text}");
-
-                Encryption encryption = new();
-
-                //Output($"PUBLIC KEY -> {encryption.PublicKey.n}");
-
-                DateTime startTimeEncrypt = DateTime.Now;
-                string EncryptedString = txtinput_testEncryption.Text.Encrypt(encryption.PublicKey, this);
-                DateTime endTimeEncrypt = DateTime.Now;
-                Output($"ENCRYPTED STRING ->");
-                Output(EncryptedString);
-                Debug.WriteLine($"Time to encrypt -> {(endTimeEncrypt - startTimeEncrypt).TotalMilliseconds}ms");
-                Output($"Decrypting using PRIVATE KEY...");
-                DateTime startTimeDecrypt = DateTime.Now;
-                string DecryptedString = EncryptedString.Decrypt(encryption.PrivateKey);
-                DateTime endTimeDecrypt = DateTime.Now;
-                Output($"DECRYPTED STRING -> {DecryptedString}");
-                Output($"Time to decrypt -> {(endTimeDecrypt - startTimeDecrypt).TotalMilliseconds}ms");
-            }
+            if (DebugWindow.IsActive) return;
+            DebugWindow.Show();
         }
     }
 }
