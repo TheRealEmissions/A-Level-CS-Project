043f13ba9e1cf666a9fb672b6b8ebc7fa0ff76f2
TheRealEmissions <unitedrealm@hotmail.com>
2021-09-23 12:28:43 +0100

    Added RecipientConnection

---
+++ The Project/Networking/RecipientConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using The_Project.Accounts;
+
+#nullable enable
+namespace The_Project.Networking
+{
+    public class RecipientConnection
+    {
+
+        public TcpClient? Client;
+
+        public RecipientConnection()
+        {
+            
+        }
+
+        public RecipientConnection(TcpClient Client)
+        {
+            this.Client = Client;
+        }
+
+        public bool ConnectTo(UserId UserId)
+        {
+            TcpClient? Client = null;
+            int PortToCheck = UserId.MinPort;
+            while (Client is null)
+            {
+                Client = CreateConnection(UserId.IP, PortToCheck, UserId.AccountId);
+            }
+            return Client is null;
+        }
+
+        public TcpClient? CreateConnection(IPAddress IP, int Port, string AccountId)
+        {
+            TcpClient Client = new(IP.ToString(), Port);
+            return !Client.Connected ? null : Client;
+        }
+    }
+}
