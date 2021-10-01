8109dec759c4999e44874d8487d23999958f3e16
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-01 14:14:00 +0100

    Added WrongPasswordException

---
+++ The Project/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Exceptions
+{
+    [Serializable]
+    class WrongPasswordException : Exception
+    {
+        public WrongPasswordException() : base("WRONG PASSWORD") { }
+    }
+}
