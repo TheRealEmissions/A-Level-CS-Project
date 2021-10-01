170f18e96b87f98f6277c3cf356cbf7e505c40b4
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-01 14:13:40 +0100

    Updated Account

--- The Project/Accounts/Account.cs
+++ The Project/Accounts/Account.cs
@@ -8,33 +8,38 @@ namespace The_Project.Accounts
 {
     public class Account
     {
-        public string Username { get; } = "";
+        public string Username { get; }
+        public string AccountId { get; }
 
         private readonly Tables Tables;
 
         // login into account and retrieve details
-        public Account(string Username, string Password, SqliteConnection Connection, Tables Tables)
+        public Account(string Username, string PasswordHash, SqliteConnection Connection, Tables Tables)
         {
-            this.Username = Username;
             this.Tables = Tables;
 
-            Database.UserAccount UserAccountDb = (Database.UserAccount)Tables.GetTable("UserAccount");
-            object[]? AccountColumns = UserAccountDb.GetAccount(Username);
-            if (AccountColumns is null)
+            UserAccount UserAccountTable = (UserAccount)Tables.GetTable("UserAccount");
+            Database.UserAccount UserAccount = new(Connection, Tables);
+
+            UserAccount.Schema? Entry = UserAccountTable.GetAccountEntry(Username);
+            if (!Entry.HasValue)
             {
                 throw new AccountNotFoundException("UserAccount");
             }
+
+            this.Username = Username;
+            AccountId = Entry.Value.AccountId;
+
+            bool PasswordIsCorrect = UserAccount.ComparePassword(this, PasswordHash);
+            if (!PasswordIsCorrect)
+            {
+                throw new WrongPasswordException();
+            }
+
         }
 
         // register new account
-        public Account(string Username, string Password, string ConfPassword, SqliteConnection Connection, Tables Tables)
-        {
-            this.Username = Username;
-            this.Tables = Tables;
-        }
-
-        // retrieve information for recipient account
-        public Account(string Username, Tables Tables)
+        public Account(string Username, string PasswordHash, string ConfPasswordHash, SqliteConnection Connection, Tables Tables)
         {
             this.Username = Username;
             this.Tables = Tables;
