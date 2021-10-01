5dda90f8423f6dc57e553e463a055c00b7f52ea2
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-01 12:23:50 +0100

    Updated Account

--- The Project/Accounts/Account.cs
+++ The Project/Accounts/Account.cs
@@ -1,27 +1,43 @@
 ﻿using Microsoft.Data.Sqlite;
+using The_Project.Database.Tables;
+using The_Project.Exceptions;
+
+#nullable enable
 
 namespace The_Project.Accounts
 {
-    public class Account : AccountHandler
+    public class Account
     {
         public string Username { get; } = "";
 
+        private readonly Tables Tables;
+
         // login into account and retrieve details
-        public Account(string Username, string Password, SqliteConnection Connection) : base(Username, Password, Connection)
+        public Account(string Username, string Password, SqliteConnection Connection, Tables Tables)
         {
             this.Username = Username;
+            this.Tables = Tables;
+
+            Database.UserAccount UserAccountDb = (Database.UserAccount)Tables.GetTable("UserAccount");
+            object[]? AccountColumns = UserAccountDb.GetAccount(Username);
+            if (AccountColumns is null)
+            {
+                throw new AccountNotFoundException("UserAccount");
+            }
         }
 
         // register new account
-        public Account(string Username, string Password, string ConfPassword, SqliteConnection Connection) : base(Username, Password, ConfPassword, Connection)
+        public Account(string Username, string Password, string ConfPassword, SqliteConnection Connection, Tables Tables)
         {
             this.Username = Username;
+            this.Tables = Tables;
         }
 
         // retrieve information for recipient account
-        public Account(string Username) : base(Username)
+        public Account(string Username, Tables Tables)
         {
             this.Username = Username;
+            this.Tables = Tables;
         }
     }
 }
