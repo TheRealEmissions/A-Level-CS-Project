e3a5a38a0fda31e418be3a2e973aa2ded31ff87f
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:28:30 +0100

    Added Networking Utils

---
+++ The Project/Networking/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Networking
+{
+    public class Utils
+    {
+
+        public static IPAddress GetIPAddress()
+        {
+            return Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+        }
+    }
+}
