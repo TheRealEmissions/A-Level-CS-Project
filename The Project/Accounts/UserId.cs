6a722f119da1acab6e0531e3fab978e6452fd1aa
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:27:34 +0100

    Updated UserId

--- The Project/Accounts/UserId.cs
+++ The Project/Accounts/UserId.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text.RegularExpressions;
 
 namespace The_Project.Accounts
 {
@@ -9,12 +10,15 @@ namespace The_Project.Accounts
         public int MaxPort { get; }
         public string AccountId { get; }
 
+        public Regex Regex { get; }
+
         public UserId(IPAddress IP, int MinPort, int MaxPort, string AccountId)
         {
             this.IP = IP;
             this.MinPort = MinPort;
             this.MaxPort = MaxPort;
             this.AccountId = AccountId;
+            Regex = new(@"/[A-I]{1,3}a[A-I]{1,3}b[A-I]{1,3}c[A-I]{1,3}ctpr[A-J]{10}[A-Z|a-z]{2}/g");
         }
     }
 }
