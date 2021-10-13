5348262583e2f5463299273774adbecca4401ff0
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:27:28 +0100

    Updated Account

--- The Project/Accounts/Account.cs
+++ The Project/Accounts/Account.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System;
 using The_Project.Database.Tables;
 using The_Project.Exceptions;
 
@@ -10,6 +11,8 @@ namespace The_Project.Accounts
     {
         public string Username { get; }
         public string AccountId { get; }
+        public int MinPort { get; }
+        public int MaxPort { get; }
 
         private readonly Tables Tables;
 
@@ -43,6 +46,34 @@ namespace The_Project.Accounts
         {
             this.Username = Username;
             this.Tables = Tables;
+
+            if (PasswordHash != ConfPasswordHash)
+            {
+                throw new PasswordHashMismatchException();
+            }
+
+            Database.UserAccount UserAccount = new(Connection, Tables);
+
+            UserId UserId = new(Networking.Utils.GetIPAddress(), new Random().Next(10000, 30000), new Random().Next(60000, 99999), GenerateAccountId());
+            MinPort = UserId.MinPort;
+            MaxPort = UserId.MaxPort;
+            AccountId = UserId.AccountId;
+
+            UserAccount.CreateEntry(Username, PasswordHash, UserId);
+        }
+
+        private static string GenerateAccountId()
+        {
+            Random Random = new();
+            string AccountId = string.Empty;
+
+            string[] Alphabet = new[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            for (int i = 0; i < 3; i++)
+            {
+                AccountId += Alphabet[Random.Next(1, 52) - 1];
+            }
+            return AccountId;
         }
     }
 }
