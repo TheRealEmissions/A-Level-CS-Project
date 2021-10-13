b55503bb7f36ae6041fd9943a75404596e51af42
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:28:17 +0100

    Added PasswordHashMismatchException

---
+++ The Project/Exceptions/PasswordHashMismatchException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Exceptions
+{
+    class PasswordHashMismatchException : Exception
+    {
+        public string Type { get; }
+
+        public PasswordHashMismatchException() : base("ACCOUNT CREATION") { }
+
+        public PasswordHashMismatchException(string Type) : this()
+        {
+            this.Type = Type;
+        }
+    }
+}
