d72f4bf6ffa1c3973c1cf8c26df52292203b3db1
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:28:06 +0100

    Added AccountCreationException

---
+++ The Project/Exceptions/AccountCreationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Project.Exceptions
+{
+    public class AccountCreationException : Exception
+    {
+       public string Type { get; }
+
+        public AccountCreationException() : base() { }
+
+        public AccountCreationException(string Type) : base(Type)
+        {
+            this.Type = Type;
+        }
+        
+    }
+}
