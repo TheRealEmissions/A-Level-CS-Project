029a0dd82bf93cd7bc0ca3838816ad8377ad8540
TheRealEmissions <unitedrealm@hotmail.com>
2021-10-13 11:28:23 +0100

    Updated Listener

--- The Project/Networking/Listener.cs
+++ The Project/Networking/Listener.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using The_Project.Accounts;
 using The_Project.Extensions;
 
@@ -27,44 +28,48 @@ namespace The_Project.Networking
             return new Random().Next(Min, Max);
         }
 
-        public RecipientConnection ListenAndConnect(string AccountId)
+        public Task<RecipientConnection> ListenAndConnect(string AccountId)
         {
-            // buffer
-            byte[] bytesBuffer = new byte[256];
-            string? accountIdBuffer;
-            RecipientConnection? Connection = null;
-
-            while (Connection is null)
+            return Task.Run(() =>
             {
-                // if no pending connection, continue loop
-                if (!Server.Pending())
-                {
-                    continue;
-                }
-                // accept pending connection
-                TcpClient Client = Server.AcceptTcpClient();
+                // buffer
+                byte[] bytesBuffer = new byte[256];
+                string? accountIdBuffer;
+                RecipientConnection? Connection = null;
 
-                // check account id
-                NetworkStream Stream = Client.GetStream();
-                while (Stream.Read(bytesBuffer, 0, bytesBuffer.Length) != 0)
+                while (Connection is null)
                 {
-                    accountIdBuffer = Encoding.UTF8.GetString(bytesBuffer);
-                    // verify account id
-                    if (accountIdBuffer == AccountId)
+                    // if no pending connection, continue loop
+                    if (!Server.Pending())
                     {
-                        Stream.Write(new BitArray(new bool[8] { true, true, true, true, true, true, true, true }).ToByteArray());
-                        Connection = new(Client);
-                        break;
+                        continue;
                     }
-                    else
+                    // accept pending connection
+                    TcpClient Client = Server.AcceptTcpClient();
+
+                    // check account id
+                    NetworkStream Stream = Client.GetStream();
+                    while (Stream.Read(bytesBuffer, 0, bytesBuffer.Length) != 0)
                     {
-                        Stream.Write(new byte[1]);
-                        Client.Close();
+                        accountIdBuffer = Encoding.UTF8.GetString(bytesBuffer);
+                        // verify account id
+                        if (accountIdBuffer == AccountId)
+                        {
+                            Stream.Write(new BitArray(new bool[8] { true, true, true, true, true, true, true, true }).ToByteArray());
+                            Connection = new(Client);
+                            break;
+                        }
+                        else
+                        {
+                            Stream.Write(new byte[1]);
+                            Client.Close();
+                        }
                     }
+                    Server.Stop();
                 }
-                Server.Stop();
-            }
-            return Connection;
+                return Connection;
+            });
+          
         }
     }
 }
