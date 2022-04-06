060e30699a1e2343ab00c93d7c0636e8f32f026b
TheRealEmissions <unitedrealm@hotmail.com>
2022-04-06 10:43:27 +0100

    changes

--- The Project/Networking/Extensions/Stream.cs
+++ The Project/Networking/Extensions/Stream.cs
@@ -14,7 +14,7 @@ namespace The_Project.Networking.Extensions
     {
         internal static void WriteData(this NetworkStream networkStream, Packet packet)
         {
-            List<byte> packetBytes = JsonSerializer.SerializeToUtf8Bytes(packet).ToList();
+            List<byte> packetBytes = JsonSerializer.SerializeToUtf8Bytes(packet, new JsonSerializerOptions {AllowTrailingCommas = true, PropertyNameCaseInsensitive = true }).ToList();
             /*packetBytes.InsertRange(0, Encoding.UTF8.GetBytes($"{packetBytes.Count}:"));*/
             packetBytes.Add(Encoding.UTF8.GetBytes("$")[0]);
             networkStream.Write(packetBytes.ToArray(), 0, packetBytes.Count);
@@ -22,7 +22,7 @@ namespace The_Project.Networking.Extensions
 
         internal static Task WriteDataAsync(this NetworkStream networkStream, Packet packet)
         {
-            List<byte> packetBytes = JsonSerializer.SerializeToUtf8Bytes(packet).ToList();
+            List<byte> packetBytes = JsonSerializer.SerializeToUtf8Bytes(packet, new JsonSerializerOptions { AllowTrailingCommas = true, PropertyNameCaseInsensitive = true }).ToList();
             /*packetBytes.InsertRange(0, Encoding.UTF8.GetBytes($"{packetBytes.Count}:"));*/
             packetBytes.Add(Encoding.UTF8.GetBytes("$")[0]);
             return networkStream.WriteAsync(packetBytes.ToArray(), 0, packetBytes.Count);
